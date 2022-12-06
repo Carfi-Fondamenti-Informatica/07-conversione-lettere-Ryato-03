9995a7dab8afe9648e7e048ce960a8d4285b92dc
Lorenzo Falbo 5640560 <116544189+Ryato-03@users.noreply.github.com>
2022-12-06 16:31:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+//
+// Created by lofal on 06/12/2022.
+//
 
+#include<iostream>
+#include "lib.h"
+
+bool Car(char &x) {
+    if (x >= 97 && x <= 122) {
+        x = x - 32;
+        return true;
+    }
+    else if (x >= 65 && x <= 90) {
+        x+=32;
+        return true;
+    }
+    else return false;
+}
